08d243bdced56d65928a1c87ac2e179d8b159383
Mohammed Al-shameri <malshameri54@gmail.com>
2023-02-23 09:59:06 +0100

    update node

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+// dotenv environment it is going to load all environment variable into process object
+//load our environment variable into process.env
+dotenv.config()
+
+console.log(process.env);
+
+const app = express()
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/weather", async (req,res) =>{
+    //proxy server
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=${process.env.SECRET_KEY}`)
+    
+    //sending response to our client
+    res.send(response.data)
+})
+
+app.listen(PORT, ()=> console.log("server is running on port:", PORT))
